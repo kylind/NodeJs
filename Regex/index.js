fdc14a6aaccc9f39d398c49a4ef9e22b2c1354ff
dai.qinglin <dai.qinglin@avature.net>
2015-03-19 23:35:02 +0800

    test

--- Regex/index.js
+++ Regex/index.js
@@ -1,24 +1,43 @@
 var fs = require('fs');
-var path=require('path');
+var path = require('path');
 
 
-var toDoFolder=process.argv[2];
-var rsFile=process.argv[3];
+var toDoFolder = process.argv[2];
+var rsFile = process.argv[3];
 
-function fetchPageFiles(dir){
-    var files=fs.readdirSync(path);
-    files.forEach(function(val){
+console.log(toDoFolder);
+console.log(rsFile);
+/*var fileList = fetchPageFiles(toDoFolder);*/
+/*fileList.forEach(function(val) {
+
+    var data=fs.readFileSync(val);
+
+    data=data + "-----------------------------------\n\r";
+
+    fs.appendFileSync(rsFile,data)
+
+});
+*/
+
+function fetchPageFiles(dir) {
+    var fileList = [];
+    var files = fs.readdirSync(path);
+    files.forEach(function(val) {
+        var filePath = dir + "/" + val;
+
+
+        var stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            var filesInDir = fetchPagefiles(filePath);
+            fileList.concat(filesInDir);
+
+        } else {
+            var extname = path.extname(filePath);
+            if (extname == "page" || extname == "nopage") {
+                fileList.push(val);
+            }
+        }
 
     })
-
+    return fileList
 }
-
-
-fs.readFile(toDoFolder, function(err, data) {
-    if (err) throw err;
-    fs.writeFile('target-ok.page', data, function(err) {
-        if (err) throw err;
-        console.log('has finished');
-    })
-
-})
