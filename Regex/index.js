e0ae5ee02fbf1602700d5bd18c32f37ac3782e97
dai.qinglin <dai.qinglin@avature.net>
2015-03-20 17:11:07 +0800

    test

--- Regex/index.js
+++ Regex/index.js
@@ -1,40 +1,50 @@
 var fs = require('fs');
 var path = require('path');
 
-
 var toDoFolder = process.argv[2];
 var rsFile = process.argv[3];
 
-console.log(toDoFolder);
-console.log(rsFile);
-/*var fileList = fetchPageFiles(toDoFolder);*/
-/*fileList.forEach(function(val) {
 
-    var data=fs.readFileSync(val);
+var fileList = fetchPageFiles(toDoFolder);
+fileList.forEach(function(val) {
 
-    data=data + "-----------------------------------\n\r";
+    var data = fs.readFileSync(val, {
+        encoding: 'utf8'
+    });
+    data = "<div>  i am div</div>";
+    var regex = new RegExp('>([;,.\\w\\t\\d]+)(?=</\\w+>)', 'igm');
+    var matched=regex.exec(data);
+    console.log(RegExp.$1);
 
-    fs.appendFileSync(rsFile,data)
+    var rsData = data.replace(RegExp.$1, 'fuck'); //RegExp.$1+    (?=<\/\w+>)
 
+    //data=data + "-----------------------------------\n\r";
+
+    fs.appendFileSync(rsFile, rsData)
 });
-*/
+
+
+function parseData() {
+
+}
+
 
 function fetchPageFiles(dir) {
+    dir = path.resolve(dir);
     var fileList = [];
-    var files = fs.readdirSync(path);
+    var files = fs.readdirSync(dir);
     files.forEach(function(val) {
         var filePath = dir + "/" + val;
 
-
         var stats = fs.statSync(filePath);
         if (stats.isDirectory()) {
-            var filesInDir = fetchPagefiles(filePath);
-            fileList.concat(filesInDir);
+            var filesInDir = fetchPageFiles(filePath);
+            fileList = fileList.concat(filesInDir);
 
         } else {
             var extname = path.extname(filePath);
-            if (extname == "page" || extname == "nopage") {
-                fileList.push(val);
+            if (extname == ".page" || extname == ".nopage") {
+                fileList.push(filePath);
             }
         }
 
