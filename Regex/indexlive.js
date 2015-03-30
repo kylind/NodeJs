a305a613702fa119290e34b9d9196ada8ddfab35
dai.qinglin <dai.qinglin@avature.net>
2015-03-30 10:38:58 +0800

    agency

---
+++ Regex/indexlive.js
@@ -0,0 +1,126 @@
+var fs = require('fs');
+var path = require('path');
+
+var toDoFolder = process.argv[2];
+var rsFile = process.argv[3];
+
+
+var fileList = fetchPageFiles(toDoFolder);
+var regexs = []
+//[\\w ,.!:'&#;\\*\\?\"\+]
+/*var regex1 = new RegExp(">(\\s*[^\\s><\\}\\{%]{2,})\\s*<[/]?", 'igm'); //<div>abc</div> google+
+var regex2 = new RegExp(">(\\s*[\\w&#]+[\\w ,.!:'&#;\\*\\?\"]{2,})\\s*\\{[\\{%]", 'igm'); //<div>abc {{ abc }} <div>abc {% abc %}
+var regex3 = new RegExp("[%\\}]\\}(\\s*[\\w&#,.]+[\\w ,.!:'&#;\\*\\?\"]{2,})\\s*<[/]?", 'igm'); //{{ abc }}abc</div> {% abc %}abc</div>
+var regex4 = new RegExp("[%\\}]\\}(\\s*[\\w&#,.]+[\\w ,.!:'&#;\\*\\?\"]{2,})\\s*\\{[\\{%]", 'igm'); //{{ abc }}abc{{ abc }}
+var regex5 = new RegExp(">(\\s*[\\w&#]+[\\s\\w ,.!:'&#;\\*\\?\"]{2,})\\s*</p>", 'igm'); //<p>sdsf</p>(multiple line p element)
+var regex5 = new RegExp("placeholder[ ]*=[ ]*\"([\\w&#]+[\\w ,.!:'&#;\\*\\?\"]{2,})\"", 'igm'); //placeholder
+*/
+var regex1 = new RegExp(">\\s*([^\\s><\\}\\{%]+[^><\\}\\{%/]{2,})\\s*<[/]?", 'igm'); //<div>abc</div> google+
+var regex2 = new RegExp(">\\s*([^\\s><\\}\\{%]+[^><\\}\\{%/]{2,})\\s*\\{[\\{%]", 'igm'); //<div>abc {{ abc }} | <div>abc {% abc %}
+var regex3 = new RegExp("[%\\}]\\}\\s*([^\\s><\\}\\{%]+[^><\\}\\{%/]{2,})\\s*</\\w+>", 'igm'); //{{ abc }}abc</div> | {% abc %}abc</div>
+var regex4 = new RegExp("[%\\}]\\}\\s*([^\\s><\\}\\{%\\=]+[^><\\}\\{%/\\=\\n\\r]{2,})\\s*\\{[\\{%]", 'igm'); //{{ abc }}abc{{ abc }}
+var regex5 = new RegExp("placeholder[ ]*=[ ]*\"([^\\s><\\}\\{%]+[\\w ]{2,})\"", 'igm'); //placeholder
+
+regexs.push(regex1);
+regexs.push(regex2);
+regexs.push(regex3);
+regexs.push(regex4);
+regexs.push(regex5);
+
+var allVariableDeclaim="";
+fileList.forEach(function(val) {
+    //var newFilename = path.dirname(val) + "/" + path.basename(val, path.extname(val)) + ".newpage";
+    var newFilename = val;
+    /*if (fs.existsSync(newFilename)) {
+        fs.unlinkSync(newFilename);
+    }
+*/
+    var data = fs.readFileSync(val, {
+        encoding: 'utf8'
+    });
+
+    if (data.trim() == "") {
+        return;
+    }
+
+    var rs = parseData(data,allVariableDeclaim);
+
+    allVariableDeclaim=rs.allVariableDeclaim;
+
+    fs.appendFileSync(rsFile, "{# ------------ " + path.basename(val) + " ------------ #}\n" + rs.variableDeclaim);
+
+    fs.writeFileSync(newFilename, rs.newData);
+
+});
+
+
+function parseData(data,allVariableDeclaim) {
+
+    var text = "";
+    var variable = "";
+    var variableDeclaim = "";
+    var variableOutput = "";
+
+    var newData = data;
+    regexs.forEach(function(regex) {
+
+        do {
+            var matched = regex.exec(data);
+            if (matched) {
+
+                text = matched[1].trim();
+                variable = text.substring(0, 50).replace(/[^0-9a-zA-Z]/g, '_');
+
+
+/*                if (allVariableDeclaim.search(new RegExp("/Start/ " + text + " /End/", "im")) == -1) {
+                    variableDeclaim = variableDeclaim + "/Name/ " + variable + " = '' /Start/ " + text + " /End/\n\n";
+                    allVariableDeclaim = allVariableDeclaim.concat(variableDeclaim);
+                }
+*/
+
+                //if (allVariableDeclaim.search(new RegExp("{# " + text + " #}","im"))==-1) {
+                if (allVariableDeclaim.indexOf("{# " + text + " #}")==-1) {
+                    variableDeclaim = variableDeclaim + "{% set " + variable + " = '"+text+"' %}{# " + text + " #}\n\n";
+                    allVariableDeclaim=allVariableDeclaim.concat(variableDeclaim);
+                }
+
+                variableOutput = matched[0].replace(matched[1], "{{ " + variable + " }}")
+                newData = newData.replace(matched[0], variableOutput);
+            }
+
+        } while (matched != null)
+
+    });
+
+    return {
+        newData: newData,
+        variableDeclaim: variableDeclaim,
+        allVariableDeclaim: allVariableDeclaim
+    };
+
+}
+
+
+function fetchPageFiles(dir) {
+    dir = path.resolve(dir);
+    var fileList = [];
+    var files = fs.readdirSync(dir);
+    files.forEach(function(val) {
+        var filePath = dir + "/" + val;
+
+        var stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            var filesInDir = fetchPageFiles(filePath);
+            fileList = fileList.concat(filesInDir);
+
+        } else {
+            var extname = path.extname(filePath);
+            var basename=path.basename(filePath, path.extname(filePath))
+            if (extname == ".page" || extname == ".nopage" || (extname == ".tpt" && basename !="debugBar" && basename!="socialShare")) {
+                fileList.push(filePath);
+            }
+        }
+
+    })
+    return fileList
+}
